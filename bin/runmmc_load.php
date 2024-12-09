73480568da6a5ca51f2f23bed2aecdbefe69fabf
ehb54 <brookes@uthscsa.edu>
2024-12-09 06:45:48 +0000

    runmmc - run locally, first commit

--- bin/runmmc_load.php
+++ bin/runmmc_load.php
@@ -44,39 +44,6 @@ if ( !$cgstate->state->flex || !count( $cgstate->state->flex ) ) {
 # $timestamp = `date '+%Y%m%d%H%M%S'`;
 $cgstate->state->mmcrunname = "run_$request->_project";
 
-$result->instructions = <<<EOT
-
-<hr>
-In the future, we plan to better integrate with SASSIE-web for MMC and other facilites, but for now, you will have to follow these instructions.
-<hr>
-<br>
-To run the MMC, you will need to go to SASSIE-web <a target=_blank href=https://sassie2.genapp.rocks>https://sassie2.genapp.rocks</a>
-<br>
-Once there, please follow these instructions carefully:
-<br>
-Log in (and register once if needed) with <b>precisely</b> the same user name you use to login to SAXS-A-FOLD, '<b>$request->_logon</b>'
-<br>
-After logging in, Navigate in SASSIE-web to Simulate->Monomer Monte Carlo, enter the values as shown below, and click <b>Submit</b>.
-<br>
-When the job is complete on SASSIE-web, return here to <i>Retrieve MMC</i>
-<br>
-Notes : for the 'reference pdb' you will have to upload it as a "Local file" to the SASSIE-web server.
-<br>
-You can first download it above under <b>Downloads</b> if needed.
-<br>
-The checkbox for 'Advanced input' should remain unchecked.
-<br>
-Please use the SASSIE-web server linked here, that is the only one currently integreated for results Retrieval.
-<br>
-The 'structure alignment range' is arbitrary and can be changed from the values shown below.
-<br>
-This is a convenience for viewing the resulting structures. Probably best to avoid flexible regions.
-<hr>
-<br>
-
-EOT;
-
-
 $result->desc                 = $cgstate->state->description;
 $result->pname                = $request->_project;
 $result->downloads            = $cgstate->state->output_load->downloads;
@@ -87,12 +54,12 @@ $result->dcdfile              = "$result->runname.dcd";
 $result->trials               = 50000;
 $result->goback               = 20;
 $result->temp                 = 300;
-$result->moltype_list_box     = "protein";
+# $result->moltype_list_box     = "protein";
 $result->numranges            = count( $cgstate->state->flex );
 $result->reslow               = str_replace( ":", ",", str_replace( ",", "-", implode( ":", $cgstate->state->flex ) ) );
 $result->dtheta               = implode( ",", array_fill( 0, $result->numranges, "30.0" ) );
 $result->residue_alignment    = "10-50";
-$result->overlap_list_box     = "heavy atoms";
+# $result->overlap_list_box     = "heavy atoms";
 
 if ( !$cgstate->save() ) {
     error_exit_hook( "Could not save important state important state information: $cgstate->errors" );
